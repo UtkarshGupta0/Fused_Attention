69387121d3224003d21207ff5af86ff016c0aad3
UtkarshGupa0 <nonugupta2010@gmail.com>
2026-09-13 19:25:42 +0530

    forgot to change the dytpe declaration to be a list of dtype 32 and dtype 16 instead of it being only 32

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -26,7 +26,7 @@ def run_benchmark_suite():
     seq_lengths = [256, 512, 1024, 2048, 4096, 8192, 16384]
     B, H, d = 2, 4, 64
     casual_modes = [True, False]
-    dtype = torch.float32
+    dtype = [torch.float32, torch.float16]
     results ={}
 
     for dtype, causal in itertools.product(dtype, casual_modes):
