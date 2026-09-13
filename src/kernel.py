c2561f3c041ab68eb98c0b3049d8c727214e0285
UtkarshGupa0 <nonugupta2010@gmail.com>
2026-09-13 20:36:25 +0530

    more bug correction

--- src/kernel.py
+++ src/kernel.py
@@ -34,7 +34,7 @@ def flash_attention_kernel(
 
     q_ptrs = q_ptr + q_base + offs_m[:, None] * stride_qn + offs_d[None, :] * stride_qd
 
-    k_ptrs = k_ptr + k_base + offs_n[:, None] * stride_kn + offs_d[None, :] * stride_kd
+    k_ptrs = k_ptr + k_base + offs_n[:, None] * stride_kn + offs_d[None, :] * stride_kn
     v_ptrs = v_ptr + v_base + offs_n[:, None] * stride_vn + offs_d[None, :] * stride_vd
 
     q_mask = (offs_m[:, None] < N) & (offs_d[None, :] < d)
@@ -53,13 +53,13 @@ def flash_attention_kernel(
     for start_n in range(0, hi, BLOCK_N):
         curr_offs_n = start_n + offs_n
 
-        k_mask = (curr_offs_n[:, None] < N) & (offs_d[None, :] < d)
+        k_mask = (offs_d[:, None] < d) & (curr_offs_n[None, :] < N)
         v_mask = (curr_offs_n[:, None] < N) & (offs_d[None, :] < d)
 
         k = tl.load(k_ptrs, mask=k_mask, other=0.0)
         v = tl.load(v_ptrs, mask=v_mask, other=0.0)
 
-        scores = tl.dot(q, tl.trans(k) , out_dtype=tl.float32) * sm_scale
+        scores = tl.dot(q, k , out_dtype=tl.float32) * sm_scale
 
         seq_mask = curr_offs_n[None, :] < N
         if CAUSAL:
