4b19a044ae95aec8b07778bd0db253a3992c8acb
UtkarshGupa0 <nonugupta2010@gmail.com>
2026-09-14 05:53:14 +0530

    last changes did not fix the code full. it should this time

--- src/kernel.py
+++ src/kernel.py
@@ -100,7 +100,7 @@ def flash_attention_kernel(
         k = tl.load(k_ptrs, mask=k_mask, other=0.0)
         v = tl.load(v_ptrs, mask=v_mask, other=0.0)
 
-        scores = tl.dot(q, k, dtype = tl.float32) * sm_scale
+        scores = tl.dot(q, k, out_dtype= tl.float32) * sm_scale
 
         seq_mask = curr_offs_n[None, :] < N
         if CAUSAL:
